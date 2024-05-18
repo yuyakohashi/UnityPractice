8a9102cb8ed4b56fb2b0d38a5e2d3cc7606a70cd
yuyakohashi <134474229+yuyakohashi@users.noreply.github.com>
2024-05-18 18:57:49 +0900

    初コミット

---
+++ Assets/Scripts/Move.cs
@@ -0,0 +1,44 @@
+using UnityEngine;
+
+public class Move : MonoBehaviour
+{
+    [SerializeField] Rigidbody _rigidBody;
+    [SerializeField] private float _speed = 10.0f;
+    public bool _isStop = false;
+
+    void Awake()
+    {
+        _rigidBody = GetComponent<Rigidbody>();
+    }
+    void Update()
+    {
+        if (!_isStop)
+        {
+            if (Input.GetKey(KeyCode.W))// Wキー（前方移動）
+            {
+                _rigidBody.velocity = transform.forward * _speed;
+            }
+            if (Input.GetKey(KeyCode.S))// Sキー（後方移動）
+            {
+                _rigidBody.velocity = -transform.forward * _speed;
+            }
+            if (Input.GetKey(KeyCode.D))// Dキー（右移動）
+            {
+                _rigidBody.velocity = transform.right * _speed;
+            }
+            if (Input.GetKey(KeyCode.A))// Aキー（左移動）
+            {
+                _rigidBody.velocity = -transform.right * _speed;
+            }
+        }
+        //https://candle-stoplight-544.notion.site/4e021f226d584730b715626436ccc330
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Gool")
+        {
+            _isStop = true;
+            _rigidBody.Sleep();
+        }
+    }
+}
