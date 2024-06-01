1e6736d0b8008cfd246c5e8d434c1586dd87e281
yuyakohashi <134474229+yuyakohashi@users.noreply.github.com>
2024-06-02 08:23:25 +0900

    左右移動と移動範囲の制限

--- Assets/Scripts/Move.cs
+++ Assets/Scripts/Move.cs
@@ -3,7 +3,9 @@ using UnityEngine;
 public class Move : MonoBehaviour
 {
     [SerializeField] Rigidbody _rigidBody;
-    [SerializeField] private float _speed = 10.0f;
+    [SerializeField] private float _speed = 8f;
+    [SerializeField][Tooltip("X軸の移動可能範囲")]private float xLimit = 9.5f;
+
     public bool _isStop = false;
 
     void Awake()
@@ -15,26 +17,23 @@ public class Move : MonoBehaviour
         if (!_isStop)
         {
             ///プレイヤーが自動で進み、ゴールまで向かう処理
-            _rigidBody.velocity = transform.forward * _speed;
+            _rigidBody.velocity = transform.forward * _speed * 2f;
 
-            /*
-            if (Input.GetKey(KeyCode.W))// Wキー（前方移動）
-            {
-                _rigidBody.velocity = transform.forward * _speed;
-            }
-            if (Input.GetKey(KeyCode.S))// Sキー（後方移動）
-            {
-                _rigidBody.velocity = -transform.forward * _speed;
-            }
             if (Input.GetKey(KeyCode.D))// Dキー（右移動）
             {
-                _rigidBody.velocity = transform.right * _speed;
+                _rigidBody.AddForce(transform.right * 10 * _speed);
             }
             if (Input.GetKey(KeyCode.A))// Aキー（左移動）
             {
-                _rigidBody.velocity = -transform.right * _speed;
+                _rigidBody.AddForce(-transform.right * 10 * _speed);
             }
-            */
+            Vector3 _currentPos = transform.position;
+
+            ///範囲を超えていたら範囲内の値を代入する
+            _currentPos.x = Mathf.Clamp(_currentPos.x, -xLimit, xLimit);
+
+            ///追加　positionをcurrentPosにする
+            transform.position = _currentPos;
         }
         //https://candle-stoplight-544.notion.site/4e021f226d584730b715626436ccc330
     }
